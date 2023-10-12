3abc65158534d17f998accaf960eb6e107664cb8
Szabó Péter <szabo.peter@students.jedlik.eu>
2023-10-12 11:25:08 +0200

    2023.10.6

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from menu import *
 from events import updateDate
+import os
 
 year = 2018
 month = 4
 day = 7
 money = 1000
+pos = 'h'
 
 while money > 0:
+    os.system("cls")
     date = updateDate(year, month, day)
     header(1000, date, 100, 0)
-    menuHouse()
+    if menuHouse() == '1':
+        map(pos, 1000, date, 100, 0) 
--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+import os
+
 def header(money, date, energy, sus):
     print("------------------------------------------")
     print(f"Pénz: {money}€\t\tDátum: {date}")
@@ -7,8 +9,32 @@ def header(money, date, energy, sus):
 def menuHouse():
     v = ''
     while v != '1' and v != '7' and v != '8' and v != '9':
-        print("1...Menjen ki az örökölt földre")
-        print("7...Menjen el a Bankba")
-        print("8...Menjen el az Áruházba")
-        print("9...Menjen el földet venni")
+        print("1... Térkép")
         v = input("Választás: ")
+    return v
+
+def map(pos, money, date, energy, sus):
+    v=''
+
+    while v != '1' and v != '2' and v != '3' and v != '4' and v != '5' and v != '6' and v != '7' and v != '8' and v != '9' and v != '10' and v != '11' and v != 'f' and v != 'b' and v != 'a' and v != 't' and v != '0':
+        os.system("cls")
+        header(1000, date, 100, 0)
+        print("1... 1.es föld", end = '\t\t')
+        print("2... 2.es föld")
+        print("3... 3.es föld", end = '\t\t')
+        print("4... 4.es föld")
+        print("5... 5.es föld", end = '\t\t')
+        print("6... 6.es föld")
+        print("7... 7.es föld", end = '\t\t')
+        print("8... 8.es föld")
+        print("9... 9.es föld", end = '\t\t')
+        print("10.. 10.es föld")
+        print("11.. 11.es föld")
+        print("\nf... Örökölt föld", end = '\t')
+        print("b... Bank")
+        print("a... Áruház", end = '\t\t')
+        print("t... Terület vétel")
+        print("\n0... Vissza")
+
+        v = input("\n\nVálasszon: ")
+    return v
