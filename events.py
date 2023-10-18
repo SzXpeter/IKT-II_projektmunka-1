bf5519efe3cc347de74421a5fc17ceb9ca955224
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2023-10-18 11:18:37 +0200

    föld vásárlás

--- events.py
+++ events.py
@@ -5,9 +5,26 @@ month = 0
 money = 1000
 pos = 'h'
 date = ""
-energy = 0
+energy = 100
 sus = 0
 inventory = []
+
+# megvan, mennyibe kerül, mérete(Ár), mi terem rajta, állapota (0 = nincs ültetve, 1 = Növekedik, 2 = Gyomlálni/öntözni kell, 3 = Kész), növekedés állapota
+# 1 = répa, 2 = krumpli, 3 = fű, 0 = még semmi
+# 1 Ár = 150 €
+
+foldOrokolt = [True, 0, 3, 0, 0, 0]
+fold1 = [False, 30000, 200, 0, 0, 0]
+fold2 = [False, 67500, 450, 0, 0, 0]
+fold3 = [False, 30000, 200, 800, 0, 0]
+fold4 = [False, 3000, 20, 0, 0, 0]
+fold5 = [False, 45000, 300, 0, 0, 0]
+fold6 = [False, 15000, 100, 0, 0, 0]
+fold7 = [False, 97500, 650, 0, 0, 0]
+fold8 = [False, 1500, 10, 0, 0, 0]
+fold9 = [False, 7500, 50, 0, 0, 0]
+fold10 = [False, 5250, 35, 0, 0, 0]
+fold11 = [False, 1500, 10, 0, 0, 0]
 
 def updateDate():
     global year, month, date
@@ -66,3 +83,19 @@ def isEnergyLow():
     if energy <= 0:
         energy = 50
         money -= money*0.15
+
+def isFarmSold(telek):
+    print(telek[0])
+    if telek[0] == False:
+        megveszed = input(f'Ez a telek még nem a tiéd, szeretnéd megvenni {telek[1]}€ -ért (i/n): ')
+        if megveszed == 'i':
+            if money >= telek[1]:
+                print('Sikeresen megvetted a telket.')
+                telek[0] = True
+                input('\nNyomj ENTER -t a folytatáshoz...')
+            else:
+                print('Nincs elég pénzed.')
+                input('\nNyomj ENTER -t a folytatáshoz...')
+    else:
+        print('Ez a te telked.')
+        input('\nNyomj ENTER -t a folytatáshoz...')
--- main.py
+++ main.py
@@ -8,7 +8,35 @@ while events.money > 0:
         case 'h':
             match menu.menuHouse():
                 case '1': 
-                    menu.map() 
+                    valasztas = menu.map()
+                    
+                    match valasztas:
+                        case '1':
+                            events.isFarmSold(events.fold1)
+                        case '2':
+                            events.isFarmSold(events.fold2)
+                        case '3':
+                            events.isFarmSold(events.fold3)
+                        case '4':
+                            events.isFarmSold(events.fold4)
+                        case '5':
+                            events.isFarmSold(events.fold5)
+                        case '6':
+                            events.isFarmSold(events.fold6)
+                        case '7':
+                            events.isFarmSold(events.fold7)
+                        case '8':
+                            events.isFarmSold(events.fold8)
+                        case '9':
+                            events.isFarmSold(events.fold9)
+                        case '10':
+                            events.isFarmSold(events.fold10)
+                        case '11':
+                            events.isFarmSold(events.fold11)
+                        case 't':
+                            events.isFarmSold(events.foldOrokolt)
+
+                    
 
         case 'a':
             events.updateDate()
